baea86df493559c46acd8e61b602cfb0dc7d0725
chanchalv <chnchlverma03@gmail.com>
2021-07-15 20:21:15 +0530

    CHECKPOINT 12

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+
+const API_URL = "https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete";
+const rapidapiKey = "ad439a2c9amshf2a0c5fa6c52cddp126b6fjsnd0013d234f79";
+const rapidapiHost = "travel-advisor.p.rapidapi.com";
+
 let viewMore = () => {
     let viewMoreButtonElement = document.getElementById("view-more-button");
     if (viewMoreButtonElement.innerText == "View More") {
---
+++ scripts/list.js
@@ -0,0 +1,4 @@
+
+const API_URL = "https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete";
+const rapidapiKey = "ad439a2c9amshf2a0c5fa6c52cddp126b6fjsnd0013d234f79";
+const rapidapiHost = "travel-advisor.p.rapidapi.com";
